0265530605f80c45d7c78785a3165d460d196808
Sylu <mortisflos@gmail.com>
2022-06-10 20:52:48 +0300

    add custom length of password

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-let allChars = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H','h','I','i','J','j','K','k','L','l','M','m',
-'N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','1','2','3','4','5','6','7','8',
-'9','0',',','!','?']
-let firstOpt = ''
-let secondOpt = ''
-let thirdOpt = ''
-let fourthOpt = ''
-let passLength = 10
-
-let passVal_1 = document.getElementById("first-option")
-let passVal_2 = document.getElementById("second-option")
-let passVal_3 = document.getElementById("third-option")
-let passVal_4 = document.getElementById("fourth-option")
-
-function passShow(){
-    passGen()
-    passVal_1.innerText = firstOpt
-    passVal_2.innerText = secondOpt
-    passVal_3.innerText = thirdOpt
-    passVal_4.innerText = fourthOpt
-}
-function passGen(){
-    for (let i = 0; i < passLength; i++)
-    {
-        firstOpt += allChars[Math.floor(Math.random() * allChars.length) + 1]
-        secondOpt += allChars[Math.floor(Math.random() * allChars.length) + 1]
-        thirdOpt += allChars[Math.floor(Math.random() * allChars.length) + 1]
-        fourthOpt += allChars[Math.floor(Math.random() * allChars.length) + 1]
-    }
+$(function(){
+    let allChars = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H','h','I','i','J','j','K','k','L','l','M','m',
+    'N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','1','2','3','4','5','6','7','8',
+    '9','0',',','!','?', '-']
+    let firstOpt = ''
+    let secondOpt = ''
+    let thirdOpt = ''
+    let fourthOpt = ''
     
-}
-passGen()
-console.log(firstOpt, secondOpt, thirdOpt, fourthOpt)
+    function passShow(){
+        passGen()
+
+        $("#first-option").text(firstOpt)
+        $("#second-option").text(secondOpt)
+        $("#third-option").text(thirdOpt)
+        $("#fourth-option").text(fourthOpt)
+
+        firstOpt = ''
+        secondOpt = ''
+        thirdOpt = ''
+        fourthOpt = ''
+    }
+    function passGen(){
+        for (let i = 0; i < $("#length-input").val(); i++)
+        {
+            firstOpt += allChars[Math.floor(Math.random() * allChars.length)]
+            secondOpt += allChars[Math.floor(Math.random() * allChars.length)]
+            thirdOpt += allChars[Math.floor(Math.random() * allChars.length)]
+            fourthOpt += allChars[Math.floor(Math.random() * allChars.length)]
+        }
+    }
+    $("#gen-button").click(function(){
+        passShow()
+    })
+});
